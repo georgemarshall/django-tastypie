19e8775042cf49daaa370a8c421e7b8a0a81d579
George Marshall <echosx@gmail.com>
2011-10-05 19:24:26 -0700

    Improved alaphanumeric http tests

--- tests/alphanumeric/tests/http.py
+++ tests/alphanumeric/tests/http.py
@@ -1,46 +1,28 @@
-import httplib
-
+from django.test.testcases import TestCase
 from django.utils import simplejson as json, unittest
 from tastypie.serializers import lxml, yaml, biplist
 
-from ..testcases import TestServerTestCase
-
-
-class HTTPTestCase(TestServerTestCase):
-    def setUp(self):
-        self.start_test_server(address='localhost', port=8001)
-
-    def tearDown(self):
-        self.stop_test_server()
-
-    def get_connection(self):
-        return httplib.HTTPConnection('localhost', 8001)
-
+class HTTPTestCase(TestCase):
     def test_get_apis_json(self):
-        connection = self.get_connection()
-        connection.request('GET', '/api/v1/', headers={'Accept': 'application/json'})
-        response = connection.getresponse()
-        connection.close()
-        data = response.read()
-        self.assertEqual(response.status, 200)
-        self.assertEqual(data, '{"products": {"list_endpoint": "/api/v1/products/", "schema": "/api/v1/products/schema/"}}')
+        response = self.client.get('/api/v1/', HTTP_ACCEPT='application/json')
+        self.assertContains(response, '{"products": {"list_endpoint": "/api/v1/products/", "schema": "/api/v1/products/schema/"}}')
 
     @unittest.skipUnless(lxml, 'lxml not installed')
     def test_get_apis_xml(self):
-        connection = self.get_connection()
-        connection.request('GET', '/api/v1/', headers={'Accept': 'application/xml'})
-        response = connection.getresponse()
-        connection.close()
-        data = response.read()
-        self.assertEqual(response.status, 200)
-        self.assertEqual(data, '<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<response><products type="hash"><list_endpoint>/api/v1/products/</list_endpoint><schema>/api/v1/products/schema/</schema></products></response>')
+        response = self.client.get('/api/v1/', HTTP_ACCEPT='application/xml')
+        self.assertContains(response, '<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<response><products type="hash"><list_endpoint>/api/v1/products/</list_endpoint><schema>/api/v1/products/schema/</schema></products></response>')
+
+    @unittest.skipUnless(yaml, 'yaml not installed')
+    def test_get_apis_yaml(self):
+        response = self.client.get('/api/v1/', HTTP_ACCEPT='text/yaml')
+        self.assertContains(response, "products: {list_endpoint: !!python/unicode '/api/v1/products/', schema: !!python/unicode '/api/v1/products/schema/'}\n")
+
+    @unittest.skipUnless(biplist, 'biplist not installed')
+    def test_get_apis_plist(self):
+        response = self.client.get('/api/v1/', HTTP_ACCEPT='application/x-plist')
+        self.assertContains(response, 'bplist00bybiplist1.0\x00\xd1\x01\x02Xproducts\xd2\x03\x04\x05\x06]list_endpointVschemao\x10\x11\x00/\x00a\x00p\x00i\x00/\x00v\x001\x00/\x00p\x00r\x00o\x00d\x00u\x00c\x00t\x00s\x00/o\x10\x18\x00/\x00a\x00p\x00i\x00/\x00v\x001\x00/\x00p\x00r\x00o\x00d\x00u\x00c\x00t\x00s\x00/\x00s\x00c\x00h\x00e\x00m\x00a\x00/\x15\x18!&4;`\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x07\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x93')
 
     def test_get_list(self):
-        connection = self.get_connection()
-        connection.request('GET', '/api/v1/products/', headers={'Accept': 'application/json'})
-        response = connection.getresponse()
-        connection.close()
-        self.assertEqual(response.status, 200)
         expected = {
             'meta': {
                 'previous': None,
@@ -94,25 +76,20 @@ class HTTPTestCase(TestServerTestCase):
                 }
             ]
         }
-        self.assertEqual(json.loads(response.read()), expected)
+        response = self.client.get('/api/v1/products/', HTTP_ACCEPT='application/json')
+        self.assertContains(response, '')
+        self.assertEqual(json.loads(response.content), expected)
 
     def test_post_object(self):
-        connection = self.get_connection()
         post_data = '{"artnr": "A76124/03", "name": "Bigwheel XXL"}'
-        connection.request('POST', '/api/v1/products/', body=post_data, headers={'Accept': 'application/json', 'Content-type': 'application/json'})
-        response = connection.getresponse()
-        self.assertEqual(response.status, 201)
-        self.assertEqual(dict(response.getheaders())['location'], 'http://localhost:8001/api/v1/products/A76124/03/')
-    
+        response = self.client.post('/api/v1/products/', data=post_data, content_type='application/json', HTTP_ACCEPT='application/json')
+        self.assertRedirects(response, '/api/v1/products/A76124/03/', 201)
+
         # make sure posted object exists
-        connection.request('GET', '/api/v1/products/A76124/03/', headers={'Accept': 'application/json'})
-        response = connection.getresponse()
-        connection.close()
-    
-        self.assertEqual(response.status, 200)
-    
-        data = response.read()
-        obj = json.loads(data)
-    
+        response = self.client.get('/api/v1/products/A76124/03/', HTTP_ACCEPT='application/json')
+        self.assertContains(response, '')
+
+        obj = json.loads(response.content)
+
         self.assertEqual(obj['name'], 'Bigwheel XXL')
         self.assertEqual(obj['artnr'], 'A76124/03')
